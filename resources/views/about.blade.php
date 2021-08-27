fbbb8a4725a4c660ee16277b4144b1368b02441b
Pape <papeseydoudia14@gmail.com>
2021-08-27 19:58:53 +0100

    Add page about-us and update link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,8 +7,8 @@
         <title>Hello City</title>
     </head>
     <body>
-        <h1>Hello from Casablanca</h1>
-        <p>It's currently {{date("H:i:s")}} PM.</p>
+       <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
+       <p><a href="/">Revenir vers la page d'accueil.</a></p>
         <footer><p>&copy; Copyright {{date("Y")}}. &middot;<a href="about-us">About Us</a></p></footer>
     </body>
 </html>
