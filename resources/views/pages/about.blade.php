5a05c76d8eb8cafcee417e28bfcf6cfe3b45abe0
Pape <papeseydoudia14@gmail.com>
2021-08-28 16:01:15 +0100

    Remove duplication by using base layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Hello City</title>
-    </head>
-    <body>
+@extends('base')
+    @section('content')
        <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
        <p><a href="/">Revenir vers la page d'accueil.</a></p>
-        <footer><p>&copy; Copyright {{date("Y")}}. &middot;</p></footer>
-    </body>
-</html>
+        
+    @endsection    
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Hello City</title>
-    </head>
-    <body>
+@extends('base')
+    @section('content')
         <h1>Hello from Casablanca</h1>
         <p>It's currently {{date("H:i:s")}} PM.</p>
-        <footer><p>&copy; Copyright {{date("Y")}}. &middot;<a href="about-us">About Us</a></p></footer>
-    </body>
-</html>
+    @endsection
