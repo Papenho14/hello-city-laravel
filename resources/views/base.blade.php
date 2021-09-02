a99ee9be295fecd576b11f8d309fda8046c044be
Pape <papeseydoudia14@gmail.com>
2021-09-02 19:09:41 +0100

    Add routes names

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,6 +9,6 @@
     <body>
         @yield('content')
 
-        <footer><p>&copy; Copyright {{date("Y")}}. &middot;<a href="about-us">About Us</a></p></footer>
+        <footer><p>&copy; Copyright {{date("Y")}}. &middot;<a href="{{route('about')}}">About Us</a></p></footer>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
     return view('pages/welcome');
-});
+})->name('welcome');
 Route::get('/about-us',function(){
     return view('pages/about');
-});
+})->name('about');
