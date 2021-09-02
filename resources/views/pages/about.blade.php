d2d466ccafdfc1395dd6df5775c93db7def3b57e
Pape <papeseydoudia14@gmail.com>
2021-09-02 12:39:08 +0100

    Popper title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,5 @@
 @extends('base')
+    @section('title','about us |hello city')
     @section('content')
        <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
        <p><a href="/">Revenir vers la page d'accueil.</a></p>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,4 +1,5 @@
 @extends('base')
+    @section('title','Hello City')
     @section('content')
         <h1>Hello from Casablanca</h1>
         <p>It's currently {{date("H:i:s")}} PM.</p>
