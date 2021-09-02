db9ffcfaa2b8c409118870b40ddec5706b36de2b
Pape <papeseydoudia14@gmail.com>
2021-09-02 18:25:18 +0100

    Avoid Duplication by using configuration

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('base')
-    @section('title','about us |hello city')
+    @section('title','about us |' .config('app.name'))
     @section('content')
        <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
        <p><a href="/">Revenir vers la page d'accueil.</a></p>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('base')
-    @section('title','Hello City')
+    @section('title',config('app.name'))
     @section('content')
         <h1>Hello from Casablanca</h1>
         <p>It's currently {{date("H:i:s")}} PM.</p>
