bb6360e6e4dfa9cbc1f6181d1d80615c2199e40c
Pape <papeseydoudia14@gmail.com>
2021-09-03 16:13:05 +0100

    Make use asset fonction help to loa images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
     @section('title','about us |' .config('app.name'))
     @section('content')
-      <img src="images/data.jpg" alt="data Flag" height="15%" width="15%">
+      <img src="/images/data.jpg" alt="data Flag" height="15%" width="15%">
        <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
        <p><a href="{{route('welcome')}}">Revenir vers la page d'accueil.</a></p>
         
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
     @section('title',config('app.name'))
     @section('content')
-        <img src="images/maroc.png" alt="Casablanca Flag" height="15%" width="15%">
+        <img src="/images/maroc.png" alt="Casablanca Flag" height="15%" width="15%">
         <h1>Hello from Casablanca</h1>
         <p>It's currently {{date("H:i:s")}} PM.</p>
     @endsection
