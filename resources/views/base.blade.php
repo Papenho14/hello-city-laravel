2790f1207fd6c4e6126b9507e1463e8703ea8357
Pape <papeseydoudia14@gmail.com>
2021-09-03 01:14:01 +0100

    Ne pas repeter le lien about us au niveau de la page about

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,6 +9,14 @@
     <body>
         @yield('content')
 
-        <footer><p>&copy; Copyright {{date("Y")}}. &middot;<a href="{{route('about')}}">About Us</a></p></footer>
+        <footer>
+            <p>
+            Copyright {{date("Y")}}.
+            @if(! Route::is('about'))
+            &copy;  &middot;<a href="{{route('about')}}">About Us</a>
+            @endif
+
+            </p>            
+        </footer>
     </body>
 </html>
