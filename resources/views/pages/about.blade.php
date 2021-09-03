ef05d699d709c1f33e543a11a462acf0ca357f91
Pape <papeseydoudia14@gmail.com>
2021-09-03 15:30:28 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,8 @@
 @extends('base')
     @section('title','about us |' .config('app.name'))
     @section('content')
+      <img src="images/data.jpg" alt="data Flag" height="15%" width="15%">
        <p>Built with &hearts; by PAPE SEYDOU DIA.</p>
-       <p><a href="/">Revenir vers la page d'accueil.</a></p>
+       <p><a href="{{route('welcome')}}">Revenir vers la page d'accueil.</a></p>
         
     @endsection    
